c6972ce4e520266772070008747ba6de0064fa1b
Gundkar <vgundkar@deloitte.com>
2025-08-13 11:30:01 +0530

    First Commit

---
+++ pdfboxutility_demo/src/main/java/pdfboxutility/pdfboxutility_demo/PdfboxutilityDemoApplication.java
@@ -0,0 +1,13 @@
+package pdfboxutility.pdfboxutility_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfboxutilityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfboxutilityDemoApplication.class, args);
+	}
+
+}
---
+++ pdfboxutility_demo/src/test/java/pdfboxutility/pdfboxutility_demo/PdfboxutilityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pdfboxutility.pdfboxutility_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfboxutilityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
